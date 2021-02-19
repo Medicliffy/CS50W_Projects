bb79eaa5bb056efefc8f0ab399f26d464b7c6d89
Ben <Medicliffy@users.noreply.github.com>
2021-02-18 22:32:56 -0700

    Basic support for displaying content. End of work 2/18

--- Project_1/wiki/encyclopedia/views.py
+++ Project_1/wiki/encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry_page(request, entry_name):
+    
+    return render(request, "encyclopedia/entry.html", {
+        "name": entry_name,
+        # TODO: fix using .capitalize() (doesn't work for CSS, etc.)
+        "content": util.get_entry(entry_name.capitalize())
+    })
