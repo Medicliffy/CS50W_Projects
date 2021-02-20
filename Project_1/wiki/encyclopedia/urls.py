821d844ce677669156420b9f98cf3730255c3205
Ben <Medicliffy@users.noreply.github.com>
2021-02-19 18:50:59 -0700

    Supports adding a new page. Styling needs work still.

--- Project_1/wiki/encyclopedia/urls.py
+++ Project_1/wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new", views.new_page, name="new_page"),
     path("<str:entry_name>", views.entry_page, name="entry"),
 ]
--- Project_1/wiki/encyclopedia/views.py
+++ Project_1/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django import forms
 
 from . import util
 
@@ -8,6 +9,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def entry_page(request, entry_name):
     
     content = util.get_entry(entry_name)
@@ -16,4 +18,46 @@ def entry_page(request, entry_name):
     return render(request, "encyclopedia/entry.html", {
         "name": entry_name,
         "content": content
+    })
+
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content", widget=forms.Textarea)
+
+
+def new_page(request):
+
+    if request.method=='POST':
+        
+        # Get data user put into new page
+        form = NewPageForm(request.POST)
+        
+        # Ensure form is valid
+        if form.is_valid():
+            
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+
+            # Ensure entry doesn't exist already
+            if title in util.list_entries():
+                # TODO: something better than this
+                # Did confirm the redirect works
+                return HttpResponseRedirect(reverse('index'))
+
+            # Save new entry
+            util.save_entry(title, content)
+
+            # Redirect
+            return HttpResponseRedirect(title)
+        
+        # Re-prompt user if form is invalid
+        else:
+            return render(request, "encyclopedia/new_page.html",{
+                "form": form
+            })
+    
+    # Render blank page if request method isn't POST
+    return render(request, "encyclopedia/new_page.html",{
+        "form": NewPageForm()
     })
